a4f31aeb6f2fadeb3c0faeade7fcabfa2ca83c58
ochorocho <rothjochen@gmail.com>
2020-12-15 21:37:22 +0000

    Fix shellcheck hints

--- backuppc-manage.sh
+++ backuppc-manage.sh
@@ -21,10 +21,10 @@ get_packagemanager () {
 	osInfo[/etc/redhat-release]="yum"
 	osInfo[/etc/debian_version]="apt"
 	
-	for f in ${!osInfo[@]}
+	for f in "${!osInfo[@]}"
 	do
 	    if [[ -f $f ]];then
-	        echo ${osInfo[$f]}
+	        echo "${osInfo[$f]}"
 	    fi
 	done
 }
@@ -85,7 +85,7 @@ install_rsync_bpc () {
 	output "Install rsync-bpc version $RSYNC_BPC ..."
 	curl -L -o rsync-bpc.tar.gz "https://github.com/backuppc/rsync-bpc/releases/download/$RSYNC_BPC/rsync-bpc-$RSYNC_BPC.tar.gz"
 	tar zxf rsync-bpc.tar.gz
-	cd rsync-bpc-$RSYNC_BPC
+	cd "rsync-bpc-$RSYNC_BPC" || exit
 	./configure
 	make
 	sudo make install
@@ -110,9 +110,9 @@ install_perl_modules () {
 
 install_backuppc () {
 	output "Install BackupPC version $BACKUPPC_VERSION ..."
-	curl -L -o BackupPC.tar.gz https://github.com/backuppc/backuppc/releases/download/$BACKUPPC_VERSION/BackupPC-$BACKUPPC_VERSION.tar.gz
+	curl -L -o BackupPC.tar.gz "https://github.com/backuppc/backuppc/releases/download/$BACKUPPC_VERSION/BackupPC-$BACKUPPC_VERSION.tar.gz"
 	tar zxf BackupPC.tar.gz
-	cd BackupPC-$BACKUPPC_VERSION	
+	cd "BackupPC-$BACKUPPC_VERSION" || exit
 	sudo perl configure.pl --batch \
 		--hostname hostname \
 		--data-dir /data/backuppc \
@@ -127,15 +127,15 @@ install_backuppc () {
 
 systemctl_configure () {
 	output "Add systemd service file ..."
-	yes | sudo cp -f BackupPC-$BACKUPPC_VERSION/systemd/backuppc.service /etc/systemd/system/backuppc.service
+	yes | sudo cp -f BackupPC-"$BACKUPPC_VERSION"/systemd/backuppc.service /etc/systemd/system/backuppc.service
 	sudo systemctl daemon-reload
 }
 
 copy_assets () {
 	output "Copy assets to $ASSETS"
 	sudo mkdir -p $ASSETS
-	sudo cp -f BackupPC-$BACKUPPC_VERSION/conf/BackupPC_* $ASSETS
-	sudo cp -f BackupPC-$BACKUPPC_VERSION/images/* $ASSETS
+	sudo cp -f BackupPC-"$BACKUPPC_VERSION"/conf/BackupPC_* $ASSETS
+	sudo cp -f BackupPC-"$BACKUPPC_VERSION"/images/* $ASSETS
 }
 
 remove_file() {
@@ -168,18 +168,18 @@ remove_folder_confirm () {
 }
 
 create_user () {
-	if id $1 &>/dev/null; then
+	if id "$1" &>/dev/null; then
 	    output "User 'backuppc' already exists ..."
 	else
 	    output "Creating '$1' user ..."
 		if [ "$PM" = "apt" ]; then
-			sudo -i addgroup --system $1
-			sudo -i adduser --system --gecos "BackupPC" --ingroup $1 --shell /bin/sh --home $HOME $1
+			sudo -i addgroup --system "$1"
+			sudo -i adduser --system --gecos "BackupPC" --ingroup "$1" --shell /bin/sh --home "$HOME" "$1"
 		fi
 
 		if [ "$PM" = "yum" ]; then
-			sudo -i groupadd --system $1
-			sudo -i useradd --system -g $1 --shell /bin/sh --home-dir $HOME $1
+			sudo -i groupadd --system "$1"
+			sudo -i useradd --system -g "$1" --shell /bin/sh --home-dir "$HOME" "$1"
 		fi
 
 		if [ "$PM" = "" ]; then
@@ -200,6 +200,7 @@ if [ "$REMOVE" = "YES" ]; then
 	RUN_DIR=$(get_config 'RunDir')
 	INSTALL_DIR=$(get_config 'InstallDir')
 
+	remove_folder_confirm "$INSTALL_DIR"
 	remove_folder_confirm "$LOG_DIR"
 	remove_folder_confirm "$RUN_DIR"
 	remove_folder_confirm "$CONF_DIR"
@@ -219,12 +220,12 @@ if [ "$INSTALL" = "YES" ]; then
 	fi
 
 	if [ "$PM" = "apt" ]; then
-		sudo $PM install gcc libacl1-dev curl perl smbclient rrdtool rsync par2 tar
+		sudo "$PM" install gcc libacl1-dev curl perl smbclient rrdtool rsync par2 tar
 	fi
 
 	if [ "$PM" = "yum" ]; then
-		sudo $PM -y install epel-release
-		sudo $PM  install gcc libacl-devel httpd-tools curl perl samba-client rrdtool rsync par2cmdline tar
+		sudo "$PM" -y install epel-release
+		sudo "$PM" install gcc libacl-devel httpd-tools curl perl samba-client rrdtool rsync par2cmdline tar
 	fi
 	
  	install_perl_modules
@@ -234,7 +235,7 @@ if [ "$INSTALL" = "YES" ]; then
 	
 	systemctl_configure
 	copy_assets
-	cleanup
+	#cleanup
 	
 	htpasswd -b -c /etc/BackupPC/passwd backuppc backuppc
 	sudo systemctl start backuppc.service
