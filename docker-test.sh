8942ed0a49ea8482ade2fd29f994303cc20daf2c
Ochorocho <rothjochen@gmail.com>
2020-12-16 21:35:23 +0100

    Add workflow for linting and install test

--- docker-test.sh
+++ docker-test.sh
@@ -3,3 +3,4 @@
 apt update && export DEBIAN_FRONTEND=noninteractive && export TZ=Europe/Berlin && apt -y install sudo tzdata make
 chmod +x /tmp/*
 sudo /tmp/backuppc-manage.sh --install --confirm --backuppc-version 4.4.0 --rsync-bpc-version 3.1.3.0
+sudo /tmp/test.sh
